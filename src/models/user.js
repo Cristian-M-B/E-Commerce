961b2937718fc0893258a3e77000ab55b34b0de5
Cristian-M-B <crisbaronetto@hotmail.com>
2022-07-15 18:24:39 -0300

    user model

---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const { Schema, model, models } = require('mongoose')
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    document: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }},
+    {
+        timestamps: false,
+        versionKey: false
+    }
+)
+
+const User = models.User || model('User', userSchema)
+
+module.exports = User
