1fc92c4a88d6b00e4a9ae6c6ea7f6f61d3d5f82e
Cristian-M-B <crisbaronetto@hotmail.com>
2022-08-04 17:48:08 -0300

    add order

--- src/context/StoreReducer.js
+++ src/context/StoreReducer.js
@@ -1,16 +1,20 @@
 export const initialState = {
     allProducts: [],
-    userInfo: {}
+    userInfo: {},
+    order: {}
 }
 
 export const actionsTypes = {
     LOAD_PRODUCTS: 'LOAD PRODUCTS',
     LOAD_USER_INFO: 'LOAD USER INFO',
     REMOVE_USER_INFO: 'REMOVE USER INFO',
+    GET_USER_INFO_LOCAL_STORAGE: 'GET USER INFO LOCAL STORAGE',
     UPDATE_USER_INFO: 'UPDATE USER INFO',
     UPDATE_FAVORITE: 'UPDATE FAVORITE',
     UPDATE_CART: 'UPDATE CART',
-    UPDATE_SHIPPING_DATA: 'UPDATE SHIPPING DATA'
+    UPDATE_SHIPPING_DATA: 'UPDATE SHIPPING DATA',
+    LOAD_ORDER: 'LOAD ORDER',
+    GET_ORDER_LOCAL_STORAGE: 'GET ORDER LOCAL STORAGE'
 }
 
 export default function StoreReducer(state, action) {
@@ -23,24 +27,34 @@ export default function StoreReducer(state, action) {
             }
 
         case actionsTypes.LOAD_USER_INFO:
+            localStorage.setItem('userInfo', JSON.stringify(action.payload))
             return {
                 ...state,
                 userInfo: action.payload
             }
 
         case actionsTypes.REMOVE_USER_INFO:
+            localStorage.removeItem('userInfo')
             return {
                 ...state,
                 userInfo: {}
             }
 
+        case actionsTypes.GET_USER_INFO_LOCAL_STORAGE:
+            return {
+                ...state,
+                userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : {}
+            }
+
         case actionsTypes.UPDATE_USER_INFO:
+            localStorage.setItem('userInfo', JSON.stringify(action.payload))
             return {
                 ...state,
                 userInfo: action.payload
             }
 
         case actionsTypes.UPDATE_FAVORITE:
+            localStorage.setItem('userInfo', JSON.stringify({ ...state.userInfo, favorites: action.payload }))
             return {
                 ...state,
                 userInfo: {
@@ -50,6 +64,7 @@ export default function StoreReducer(state, action) {
             }
 
         case actionsTypes.UPDATE_CART:
+            localStorage.setItem('userInfo', JSON.stringify({ ...state.userInfo, cart: action.payload }))
             return {
                 ...state,
                 userInfo: {
@@ -59,6 +74,7 @@ export default function StoreReducer(state, action) {
             }
 
         case actionsTypes.UPDATE_SHIPPING_DATA:
+            localStorage.setItem('userInfo', JSON.stringify({ ...state.userInfo, shippingData: action.payload }))
             return {
                 ...state,
                 userInfo: {
@@ -66,6 +82,20 @@ export default function StoreReducer(state, action) {
                     shippingData: action.payload
                 }
             }
+
+        case actionsTypes.LOAD_ORDER:
+            localStorage.setItem('order', JSON.stringify(action.payload))
+            return {
+                ...state,
+                order: action.payload
+            }
+
+        case actionsTypes.GET_ORDER_LOCAL_STORAGE:
+            return {
+                ...state,
+                order: localStorage.getItem('order') ? JSON.parse(localStorage.getItem('order')) : {}
+            }
+
 
         default:
             return state
