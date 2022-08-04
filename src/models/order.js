ae83bda9695394a700bdf05e7e5affb0076bc09c
Cristian-M-B <crisbaronetto@hotmail.com>
2022-08-04 17:50:09 -0300

    add order model

---
+++ src/models/order.js
@@ -0,0 +1,43 @@
+const { Schema, model, models } = require('mongoose')
+
+const orderSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    products: {
+        type: Array,
+        required: true,
+    },
+    deliveryMode: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    paymentID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    paymentStatus: {
+        type: String,
+        required: true
+    },
+    paymentType: {
+        type: String
+    },
+    preferenceID: {
+        type: String
+    }},
+    {
+        timestamps: false,
+        versionKey: false
+    }
+)
+
+const Order = models.Order || model('Order', orderSchema)
+
+module.exports = Order
