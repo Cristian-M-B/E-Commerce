0399df8d91cfa2e35f54cb79a9b52f7660056e9d
Cristian-M-B <crisbaronetto@hotmail.com>
2022-08-23 11:59:25 -0300

    modification

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,5 @@
 export function parseCurrency(value) {
-    return value.toLocaleString('es-Ar', {
+    return value?.toLocaleString('es-Ar', {
         style: 'currency',
         currency: 'ARS'
     })
