3a0b215e78cace39afe891386451afca55dfd271
Cristian-M-B <crisbaronetto@hotmail.com>
2022-08-23 18:16:49 -0300

    rafactoring

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,16 +1,37 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
-let connectionDB = () => {
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    mongoose.connection.on('open', (_) => {
-        console.log('Database connected');
-    });
-    mongoose.connection.on('error', (error) => {
-        console.log(error);
-    });
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local'
+    )
 }
 
-module.exports = connectionDB
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function connectionDB() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+
+    cached.conn = await cached.promise
+
+    return cached.conn
+}
+
+export default connectionDB
