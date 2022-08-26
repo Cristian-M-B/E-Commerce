2d7a4c3999b960d840662436173cf8af9c1b5532
Cristian-M-B <crisbaronetto@hotmail.com>
2022-08-26 11:13:06 -0300

    add reviews component

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,19 +1,21 @@
 import Head from 'next/head'
 import connectionDB from '../../utils/db'
 import Product from '../../models/product'
+import Order from '../../models/order'
 import Layout from '../../components/Layout'
+import Reviews from '../../components/Reviews'
 import { Grid, Typography, Stack } from '@mui/material'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-export default function Detail({ product }) {
+export default function Detail({ product, orders }) {
     return (
         <Layout>
             <Head>
                 <title>E-Commerce | {product.name}</title>
             </Head>
             <Grid container direction='row' justifyContent='space-evenly'>
-                <Carousel width='350px'>
+                <Carousel showStatus={false} infiniteLoop={true} width='350px'>
                     {product.images.map((image, index) => {
                         return <img key={index}
                             src={image}
@@ -22,12 +24,13 @@ export default function Detail({ product }) {
                     })}
                 </Carousel>
                 <Stack direction='column'>
-                    <Typography variant='h4' component='h4' align='center' style={{marginBottom: '4vh'}}>{product.name}</Typography>
-                    <Typography style={{marginBottom: '2vh'}}>{product.description}</Typography>
+                    <Typography variant='h4' component='h4' align='center' style={{ marginBottom: '4vh' }}>{product.name}</Typography>
+                    <Typography style={{ marginBottom: '2vh' }}>{product.description}</Typography>
                     <Typography>Precio: ${product.price}</Typography>
                     <Typography>Stock: {product.stock}</Typography>
                 </Stack>
             </Grid>
+            <Reviews orders={orders} />
         </Layout>
     )
 }
@@ -36,11 +39,13 @@ export async function getServerSideProps({ params }) {
 
     await connectionDB();
 
-    const product = await Product.findById(params.id).lean()
+    const product = await Product.findById(params.id).lean();
+    const orders = await Order.find({}).populate('user').lean();
 
     return {
         props: {
-            product: JSON.parse(JSON.stringify(product))
+            product: JSON.parse(JSON.stringify(product)),
+            orders: JSON.parse(JSON.stringify(orders))
         }
     }
 }
