8962045bca79117f94f5123d6d3c8e0de6dfc48f
Cristian-M-B <crisbaronetto@hotmail.com>
2022-09-30 11:26:27 -0300

    add status change

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -17,8 +17,14 @@ export default async function handler(req, res) {
             deliveryMode: req.body.deliveryMode
         })
         const orderSaved = await newOrder.save();
-        const myOrder = await Order.findById( orderSaved._id ).populate('user').lean();
+        const myOrder = await Order.findById(orderSaved._id).populate('user').lean();
         transporter.sendMail(order(myOrder))
         res.status(200).json(orderSaved);
+    }
+
+    if (req.method === 'PUT') {
+        await Order.findByIdAndUpdate(req.body.id, { paymentStatus: req.body.status })
+        const listOrders = await Order.find({}).populate('user').lean()
+        res.status(200).json(listOrders)
     }
 }
