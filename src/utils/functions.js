7a3cfec91d314aea78eb27e8df3c36294896acac
Cristian-M-B <crisbaronetto@hotmail.com>
2022-09-30 11:23:50 -0300

    add paymentStatus and paymentStatusColor functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -4,3 +4,23 @@ export function parseCurrency(value) {
         currency: 'ARS'
     })
 }
+
+export function paymentStatus(status) {
+    if (status === 'preparing') return 'Preparando'
+    if (status === 'traveling') return 'Viajando'
+    if (status === 'delivered') return 'Entregado'
+    if (status === 'approved') return 'Pagado'
+    if (status === 'pending') return 'Pendiente'
+    if (status === 'in_process') return 'Pendiente'
+    return 'Cancelado'
+}
+
+export function paymentStatusColor(status) {
+    if (status === 'preparing') return 'info.light'
+    if (status === 'traveling') return 'info.main'
+    if (status === 'delivered') return 'info.dark'
+    if (status === 'approved') return 'success.main'
+    if (status === 'pending') return 'warning.main'
+    if (status === 'in_process') return 'warning.main'
+    return 'error.main'
+}
