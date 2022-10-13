3b01298dcb55c4f179f5370d3c879e00c14ccd4f
Cristian-M-B <crisbaronetto@hotmail.com>
2022-10-13 13:35:40 -0300

    add method post, put and delete

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -2,8 +2,37 @@ import connectionDB from '../../utils/db'
 import Product from '../../models/product'
 
 export default async function handler(req, res) {
-  
   await connectionDB();
-  const allProducts = await Product.find({}).lean();
-  res.status(200).json(allProducts);
+
+  if (req.method === 'GET') {
+    const allProducts = await Product.find({}).lean();
+    res.status(200).json(allProducts);
+  }
+
+  if (req.method === 'POST') {
+    const newProduct = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      categories: req.body.categories,
+      images: req.body.images
+    })
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct)
+  }
+
+  if (req.method === 'PUT') {
+    const { id } = req.query
+    await Product.findByIdAndUpdate(id, { ...req.body })
+    const updatedProduct = await Product.findById(id).lean()
+    res.status(200).json(updatedProduct)
+  }
+
+  if (req.method === 'DELETE') {
+    const { id } = req.query;
+    await Product.findByIdAndDelete(id);
+    const listProduct = await Product.find({}).lean();
+    res.status(200).json(listProduct);
+  }
 }
