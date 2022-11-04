9801dcd86242c1b0f021343be0fb65358a31b3c4
Cristian-M-B <crisbaronetto@hotmail.com>
2022-11-04 15:02:17 -0300

    modification

--- src/pages/api/notifications.js
+++ src/pages/api/notifications.js
@@ -5,18 +5,18 @@ import { transporter, notification } from '../../utils/email'
 
 export default async function handler(req, res) {
     const id = req.body?.data?.id
-    if (id) {
-        const { data } = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
-            headers: {
-                'Authorization': `Bearer ${process.env.ACCESS_TOKEN_MP}`
-            }
-        })
-        await connectionDB()
-        await Order.updateOne({ paymentID: id }, { paymentStatus: data.status })
-        const order = await Order.findOne({ paymentID: id }).populate('user').lean()
-        if (order) {
-            transporter.sendMail(notification(order))
-        }
-    }
+    // if (id) {
+    //     const { data } = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
+    //         headers: {
+    //             'Authorization': `Bearer ${process.env.ACCESS_TOKEN_MP}`
+    //         }
+    //     })
+    //     await connectionDB()
+    //     await Order.updateOne({ paymentID: id }, { paymentStatus: data.status })
+    //     const order = await Order.findOne({ paymentID: id }).populate('user').lean()
+    //     if (order) {
+    //         transporter.sendMail(notification(order))
+    //     }
+    // }
     res.status(200).send('OK')
 }
