c34d7e9effc7419f3bd8bd8447b2157c9a35f141
Cristian-M-B <crisbaronetto@hotmail.com>
2022-11-23 17:11:49 -0300

    modification

--- src/pages/api/notifications.js
+++ src/pages/api/notifications.js
@@ -3,20 +3,23 @@ import Order from '../../models/order'
 import axios from 'axios'
 import { transporter, notification } from '../../utils/email'
 
+let noti = []
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
+        noti.push(req.body)
         const id = req.body.data.id
-        // const { data } = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
-        //     headers: {
-        //         'Authorization': `Bearer ${process.env.ACCESS_TOKEN_MP}`
-        //     }
-        // })
-        // await connectionDB()
-        // await Order.updateOne({ paymentID: id }, { paymentStatus: data.status })
-        // const order = await Order.findOne({ paymentID: id }).populate('user').lean()
-        // if (order) {
-        //     transporter.sendMail(notification(order))
-        // }
+        const { data } = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${process.env.ACCESS_TOKEN_MP}`
+            }
+        })
+        await connectionDB()
+        await Order.updateOne({ paymentID: id }, { paymentStatus: data.status })
+        const order = await Order.findOne({ paymentID: id }).populate('user').lean()
+        if (order) {
+            transporter.sendMail(notification(order))
+        }
         transporter.sendMail({
             from: `E-Commerce <${process.env.EMAIL}>`,
             to: 'crismaxbar@gmail.com',
@@ -24,5 +27,9 @@ export default async function handler(req, res) {
             html: `${id}`
         })
         res.status(200).send('OK')
+    }
+
+    if(req.method === 'GET') {
+        res.status(200).json(noti)
     }
 }
