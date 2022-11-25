ffe642b3822574a74d82725063a545ca6b290867
Cristian-M-B <crisbaronetto@hotmail.com>
2022-11-25 15:30:33 -0300

    modification

--- src/pages/order/pending.js
+++ src/pages/order/pending.js
@@ -1,17 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../../components/Layout'
 import axios from 'axios'
 import { useStore, useDispatch } from '../../context/StoreProvider'
 import { actionsTypes } from '../../context/StoreReducer'
-import { Grid, Typography, CircularProgress } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 
 export default function Pending({ query }) {
-    const [loading, setLoading] = useState(true);
-    const { userInfo, order } = useStore();
-    const { userID, products, deliveryMode } = order;
-    const dispatch = useDispatch();
-    const currentDate = new Date().toLocaleString();
+    const { userInfo, order } = useStore()
+    const { userID, products, deliveryMode } = order
+    const dispatch = useDispatch()
+    const currentDate = new Date().toLocaleString()
     const mp = {
         preferenceID: query.preference_id,
         paymentType: query.payment_type,
@@ -24,33 +23,24 @@ export default function Pending({ query }) {
     }, [userID])
 
     async function newOrder() {
+        dispatch({ type: actionsTypes.UPDATE_CART, payload: [] })
         const order = await axios.post(`/api/order`, { userID, products, mp, currentDate, deliveryMode })
-        const { data } = await axios.delete(`/api/user?user=${userInfo?._id}&emptyCart=${true}`)
-        dispatch({ type: actionsTypes.UPDATE_CART, payload: data })
-        products.map(async (product) => (
-            await axios.put(`/api/products?id=${product.id}&subtractStock=${true}`, { subtractStock: product.quantity })
-        ))
-        setLoading(false)
+        await axios.delete(`/api/user?user=${userInfo?._id}&emptyCart=${true}`)
+        await Promise.all(products.map(async (product) => {
+            return await axios.put(`/api/products?id=${product.id}&subtractStock=${true}`, { subtractStock: product.quantity })
+        }))
     }
+
     return (
         <Layout>
             <Head>
                 <title>E-Commerce | Pago Pendiente</title>
             </Head>
             <Grid container direction='column' justifyContent='center' alignItems='center' style={{ minHeight: '75vh' }}>
-                {loading
-                    ? <>
-                        <Typography variant='h5' component='h5'>¡Estamos procesando tu compra!</Typography>
-                        <Typography variant='h6' component='h6'>Por favor, aguarda un momento.</Typography>
-                        <CircularProgress color='primary' sx={{ marginTop: '5vh' }} />
-                    </>
-                    : <>
-                        <img src='/warning.png' width='100' height='100' style={{ marginBottom: '50px' }} />
-                        <Typography variant='h5' component='h5' align='center'>¡Tú compra esta pendiente de pago!</Typography>
-                        <Typography variant='h6' component='h6' align='center'>Una vez que termines con el proceso de pago, empezaremos a preparar tu pedido.</Typography>
-                        <Typography variant='h6' component='h6' style={{ marginTop: '4vh' }}>¡Muchas gracias por elegirnos!</Typography>
-                    </>
-                }
+                <img src='/warning.png' width='100' height='100' style={{ marginBottom: '50px' }} />
+                <Typography variant='h5' component='h5' align='center'>¡Tú compra esta pendiente de pago!</Typography>
+                <Typography variant='h6' component='h6' align='center'>Una vez que termines con el proceso de pago, empezaremos a preparar tu pedido.</Typography>
+                <Typography variant='h6' component='h6' style={{ marginTop: '4vh' }}>¡Muchas gracias por elegirnos!</Typography>
             </Grid>
         </Layout>
     )
--- src/pages/order/success.js
+++ src/pages/order/success.js
@@ -1,17 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Layout from '../../components/Layout'
 import axios from 'axios'
 import { useStore, useDispatch } from '../../context/StoreProvider'
 import { actionsTypes } from '../../context/StoreReducer'
-import { Grid, Typography, CircularProgress } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 
 export default function Success({ query }) {
-    const [loading, setLoading] = useState(true);
-    const { userInfo, order } = useStore();
-    const { userID, products, deliveryMode } = order;
-    const dispatch = useDispatch();
-    const currentDate = new Date().toLocaleString();
+    const { userInfo, order } = useStore()
+    const { userID, products, deliveryMode } = order
+    const dispatch = useDispatch()
+    const currentDate = new Date().toLocaleString()
     const mp = {
         preferenceID: query.preference_id,
         paymentType: query.payment_type,
@@ -24,13 +23,12 @@ export default function Success({ query }) {
     }, [userID])
 
     async function newOrder() {
+        dispatch({ type: actionsTypes.UPDATE_CART, payload: [] })
         const order = await axios.post(`/api/order`, { userID, products, mp, currentDate, deliveryMode })
-        const { data } = await axios.delete(`/api/user?user=${userInfo?._id}&emptyCart=${true}`)
-        dispatch({ type: actionsTypes.UPDATE_CART, payload: data })
+        await axios.delete(`/api/user?user=${userInfo?._id}&emptyCart=${true}`)
         await Promise.all(products.map(async (product) => {
             return await axios.put(`/api/products?id=${product.id}&subtractStock=${true}`, { subtractStock: product.quantity })
         }))
-        setLoading(false)
     }
 
     return (
@@ -39,20 +37,11 @@ export default function Success({ query }) {
                 <title>E-Commerce | Pago Exitoso</title>
             </Head>
             <Grid container direction='column' justifyContent='center' alignItems='center' style={{ minHeight: '75vh' }}>
-                {loading
-                    ? <>
-                        <Typography variant='h5' component='h5'>¡Estamos procesando tu compra!</Typography>
-                        <Typography variant='h6' component='h6'>Por favor, aguarda un momento.</Typography>
-                        <CircularProgress color='primary' sx={{ marginTop: '5vh' }} />
-                    </>
-                    : <>
-                        <img src='/success.png' width='100' height='100' style={{ marginBottom: '50px' }} />
-                        <Typography variant='h5' component='h5'>¡La operación fue un exito!</Typography>
-                        <Typography variant='h6' component='h6' align='center'>A la brevedad comenzaremos a preparar tu pedido.</Typography>
-                        <Typography variant='h6' component='h6' align='center'>Además recibirás un email con el detalle de tu compra.</Typography>
-                        <Typography variant='h6' component='h6' style={{ marginTop: '4vh' }}>¡Muchas gracias por elegirnos!</Typography>
-                    </>
-                }
+                <img src='/success.png' width='100' height='100' style={{ marginBottom: '50px' }} />
+                <Typography variant='h5' component='h5'>¡La operación fue un exito!</Typography>
+                <Typography variant='h6' component='h6' align='center'>A la brevedad comenzaremos a preparar tu pedido.</Typography>
+                <Typography variant='h6' component='h6' align='center'>Además recibirás un email con el detalle de tu compra.</Typography>
+                <Typography variant='h6' component='h6' style={{ marginTop: '4vh' }}>¡Muchas gracias por elegirnos!</Typography>
             </Grid>
         </Layout>
     )
